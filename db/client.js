514eff003355f962d5bc419de6266db926861028
Tyler Woyshner <tylerwoyshner@Tylers-MacBook-Pro.local>
2024-06-05 21:31:00 -0400

    first commit

--- db/client.js
+++ db/client.js
@@ -1,6 +1,6 @@
 const { Client } = require('pg');
 
-const connectionString = process.env.DATABASE_URL || 'https://localhost:5432/gamestore';
+const connectionString = process.env.DATABASE_URL || 'postgresql://postgres:postgres@localhost:5432/gamestore';
 
 const client = new Client({
   connectionString,
--- db/videoGames.js
+++ db/videoGames.js
@@ -6,7 +6,9 @@ const REPLACE_ME = 'HELP REPLACE ME!!!!';
 // GET - /api/video-games - get all video games
 async function getAllVideoGames() {
     try {
-        const { rows: videoGames } = await client.query(REPLACE_ME);
+        const { rows: videoGames } = await client.query(`
+        SELECT * FROM videogames;
+    `);
         return videoGames;
     } catch (error) {
         throw new Error("Make sure you have replaced the REPLACE_ME placeholder.")
@@ -28,17 +30,51 @@ async function getVideoGameById(id) {
 
 // POST - /api/video-games - create a new video game
 async function createVideoGame(body) {
-    // LOGIC GOES HERE
+    const { name, description, price, inStock, isPopular, imgUrl } = body;
+    try {
+        const { rows: [videoGame] } = await client.query(`
+
+            INSERT INTO videogames(name, description, price, "inStock", "isPopular", "imgUrl")
+            VALUES($1, $2, $3, $4, $5, $6)
+            RETURNING *;
+        `, [name, description, price, inStock, isPopular, imgUrl]);
+        return videoGame;
+    } catch (error) {
+        throw error;
+    }
 }
 
 // PUT - /api/video-games/:id - update a single video game by id
 async function updateVideoGame(id, fields = {}) {
-    // LOGIC GOES HERE
+    const setString = Object.keys(fields).map((key, index) => `"${key}"=$${index + 1}`).join(', ');
+    if (setString.length === 0) {
+        return;
+    }
+    try {
+        const { rows: [videoGame] } = await client.query(`
+            UPDATE videogames
+            SET ${setString}
+            WHERE id=${id}
+            RETURNING *;
+        `, Object.values(fields));
+        return videoGame;
+    } catch (error) {
+        throw error;
+    }
 }
 
 // DELETE - /api/video-games/:id - delete a single video game by id
 async function deleteVideoGame(id) {
-    // LOGIC GOES HERE
+    try {
+        const { rows: [videoGame] } = await client.query(`
+            DELETE FROM videogames
+            WHERE id=$1
+            RETURNING *;
+        `, [id]);
+        return videoGame;
+    } catch (error) {
+        throw error;
+    }
 }
 
 module.exports = {
